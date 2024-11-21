53d376db044025ad92cad23ccaae4755b8ef4cea
melvinRenversez <120313822+melvinRenversez@users.noreply.github.com>
2024-11-21 18:31:25 +0100

    clear

--- src/main/java/fr/mrl/Main.java
+++ src/main/java/fr/mrl/Main.java
@@ -1,60 +1,63 @@
 package fr.mrl;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void attack(Player attacker, Player defender) {
-        int baseDamage = attacker.getForce() / 2;
-        int agilityAdd = randomNumber(0, attacker.getAgility());
-        int agilityReduce = randomNumber(0, defender.getAgility());
-
-        int damage = Math.max(baseDamage + agilityAdd - agilityReduce, 0);
-
-        defender.setPv(defender.getPv() - damage);
-
-        System.out.println(attacker.getName() + " attaque " + defender.getName() + " et inflige " + damage + " dégâts.");
-    }
-
-    public static int randomNumber(int min, int max) {
-        Random random = new Random();
-        return random.nextInt((max - min) + 1) + min;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Player player1 = new Player("Frank", 100, 80, 5000);
-        Player player2 = new Player("Baptiste", 120, 70, 3500);
-
-        System.out.println("Bienvenue dans l'arène !");
-        player1.sayAll();
-        player2.sayAll();
+        for (int i = 0; i < 100; i++) {
+            System.out.println();
+        }
 
         System.out.println("----------------------------------------");
+        System.out.println("Bienvenue dans l'arène !");
+        System.out.println("----------------------------------------");
+        
+        System.err.println();
+        
+        System.out.println("----------- Player 1 -----------");
+        Player player1 = new Player();
+        player1.describe();
+        
+        System.out.println("----------- Player 2 -----------");
+        Player player2 = new Player();
+        player2.describe();
+        
+        
+        
+        
+        System.out.println("");
+        System.out.println("----------------------------------------");
         System.out.println("Le combat commence !");
-
+        System.out.println("----------------------------------------");
+        
         int round = 1;
-
+        
         while (player1.getPv() > 0 && player2.getPv() > 0) {
+            System.out.println("");
             System.out.println("------------ Round " + round + " ------------");
 
             // Tour du joueur 1
             System.out.println(player1.getName() + " attaque !");
-            attack(player1, player2);
+            scanner.nextLine();
+            player1.attack(player2);
             if (player2.getPv() <= 0) break;
-
+            
             System.err.println("");
-
+            
             // Tour du joueur 2
             System.out.println(player2.getName() + " attaque !");
-            attack(player2, player1);
+            scanner.nextLine();
+            player2.attack(player1);
 
             round++;
         }
 
-        System.out.println("----------------------------------------");
+        System.err.println();
+        System.err.println();
+
         if (player1.getPv() > 0) {
             System.out.println(player1.getName() + " a gagné le combat !");
         } else {
@@ -63,4 +66,5 @@ public class Main {
 
         scanner.close();
     }
+    
 }
--- src/main/java/fr/mrl/Player.java
+++ src/main/java/fr/mrl/Player.java
@@ -1,17 +1,54 @@
 package fr.mrl;
 
-public class Player {
+import java.util.Scanner;
+import java.util.Random;
 
-    private String name;
+public class Player {
+    Scanner scanner = new Scanner(System.in);
+
+    private String name = "";
     private int force;
     private int agility;
     private int pv;
 
-    public Player(String name, int force, int agility, int pv) {
-        this.name = name;
-        this.force = force;
-        this.agility = agility;
-        this.pv = pv;
+    public Player() {
+
+        while (name ==  "") {
+            System.out.print(" -Your name ");
+            this.name = scanner.nextLine();
+        }
+        while (force < 1) {
+            try {
+                System.out.print(" -Your force ");
+                this.force = scanner.nextInt();
+            }
+            catch (Exception e) {
+                System.out.println("Veuillez entrer un nombre valide.");
+                scanner.next();
+            }
+        }
+
+        while (agility < 1) {
+            try {
+                System.out.print(" -Your agility ");
+                this.agility = scanner.nextInt();
+            }
+            catch (Exception e) {
+                System.out.println("Veuillez entrer un nombre valide.");
+                scanner.next();
+            }
+        }
+        while (pv < 1) {
+            try {
+                System.out.print(" -Your Pv ");
+                this.pv = scanner.nextInt();
+            }
+            catch (Exception e) {
+                System.out.println("Veuillez entrer un nombre valide.");
+                scanner.next();
+            }
+        }
+        System.err.println();
     }
 
     public String getName() {
@@ -32,13 +69,35 @@ public class Player {
 
     public void setPv(int pv) {
         this.pv = pv;
+        if (pv <= 0) {
+            this.pv = 0;
+        }
     }
 
-    public void sayAll() {
+    public void describe() {
         System.out.println("Je suis " + name);
-        System.out.println("Force : " + force);
-        System.out.println("Agilité : " + agility);
-        System.out.println("Points de vie : " + pv);
-        System.out.println("----------------------------------------");
+        System.out.println(" -Force : " + force);
+        System.out.println(" -Agilité : " + agility);
+        System.out.println(" -Points de vie : " + pv);
+        System.err.println();
+    }
+
+    public void attack(Player target){
+        int baseDamage = force / 2;
+        int agilityAdd = randomNumber(0, agility);
+        int agilityReduce = randomNumber(0, target.getAgility());
+
+        int damage = Math.max(baseDamage + agilityAdd - agilityReduce, 0);
+
+        System.out.println(name + " attaque " + target.getName() + " et inflige " + damage + " dégâts.");
+        System.out.println(target.getPv() + " - " + damage);
+        
+        target.setPv(target.getPv() - damage);
+        System.out.println("il reste " + target.getPv() + " Pv a " + target.getName());
+    }
+
+    public static int randomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
     }
 }
